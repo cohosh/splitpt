46177393ef6f219e1d7f4291e7a0a36e8df1044d
annalorimer <annalorimer@uchicago.edu>
2024-01-16 16:12:12 -0600

    handle client ending before socksAcceptLoop() can execute; update torrc

--- client/splitpt.go
+++ client/splitpt.go
@@ -5,16 +5,16 @@ import (
 	"flag"
 	"fmt"
 	"io"
-//	"io/ioutil"
+	"io/ioutil"
 	"log"
 	"net"
 	"os"
-//	"os/signal"
+	"os/signal"
 //	"path/filepath"
 //	"strconv"
 //	"strings"
 //	"sync"
-//	"syscall"
+	"syscall"
 
 	pt "gitlab.torproject.org/tpo/anti-censorship/pluggable-transports/goptlib"
 	spt "anticensorshiptrafficsplitting/splitpt/client/lib"
@@ -41,7 +41,8 @@ func copyLoop(socks, sptconn io.ReadWriter) {
 }
 
 // TODO handle the socks cxn between splitpt and the pt being used to transport traffic
-func socksAcceptLoop(ln *pt.SocksListener, config spt.ClientConfig) {
+func socksAcceptLoop(ln *pt.SocksListener, config spt.ClientConfig) error {
+	log.Printf("socksAcceptLoop()")
 	defer ln.Close()
 	for {
 		conn, err := ln.AcceptSocks()
@@ -55,9 +56,11 @@ func socksAcceptLoop(ln *pt.SocksListener, config spt.ClientConfig) {
 		// [AHL] should be replaced with copyLoop later
 		go handler(conn)
 	}	
+	return nil
 }
 
 func handler(conn *pt.SocksConn) error {
+	log.Printf("handler()")
 	defer conn.Close()
 	remote, err := net.Dial("tcp", conn.Req.Target)
 	if err != nil {
@@ -83,14 +86,17 @@ func main() {
 	// Logging
 	log.SetFlags(log.LstdFlags | log.LUTC)
 
+	var logOutput = ioutil.Discard
 	if *logFilename != "" {
 		logFile, err := os.OpenFile(*logFilename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer logFile.Close()
+		logOutput = logFile
 	}
-
+	log.SetOutput(logOutput)
+	log.Println("--- Starting SplitPT ---")
 	// splitpt setup
 
 	config := spt.ClientConfig {
@@ -100,30 +106,39 @@ func main() {
 	// begin goptlib client process
 	ptInfo, err := pt.ClientSetup(nil)
 	if err != nil {
+		log.Printf("ClientSetup failed")
 		os.Exit(1)
 	}
 
 	if ptInfo.ProxyURL != nil {
 		pt.ProxyError(fmt.Sprintf("proxy %s is not supported", ptInfo.ProxyURL))
+		log.Printf("Proxy is nor supported")
 		os.Exit(1)
 	}
 	for _, methodName := range ptInfo.MethodNames { 
-		// [AHL] I think methodnames is fetched from the torrc?
 		switch methodName {
 			case "splitpt":
+				log.Printf("splitpt method found")
 				ln, err := pt.ListenSocks("tcp", "127.0.0.1:0")
 				if err != nil {
 					pt.CmethodError(methodName, err.Error())
 					break
 				}
+				log.Printf("Started SOCKS listenener at %v", ln.Addr())
 				go socksAcceptLoop(ln, config)
 				pt.Cmethod(methodName, ln.Version(), ln.Addr())
 			default:
+				log.Printf("no such method splitpt")
 				pt.CmethodError(methodName, "no such method")
 		}
 	}
 	pt.CmethodsDone()
 
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM)
+
+	<-sigChan
+	log.Printf("stopping splitpt")
 	log.Println("SplitPT is done")
 
 }
