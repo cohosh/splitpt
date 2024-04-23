5911a04950b3b218a8f0abef8a0ae62bfede7cd5
annalorimer <annalorimer@uchicago.edu>
2024-04-23 10:29:42 -0500

    obfs4 setup

--- pt-setup/obfs4-client.sh
+++ pt-setup/obfs4-client.sh
@@ -1,4 +1,5 @@
 export TOR_PT_MANAGED_TRANSPORT_VER=1
 export TOR_PT_CLIENT_TRANSPORTS=obfs4
+export TOR_PT_STATE_LOCATION=client-state/
 
 lyrebird 
--- pt-setup/obfs4-server.sh
+++ pt-setup/obfs4-server.sh
@@ -2,3 +2,6 @@ export TOR_PT_MANAGED_TRANSPORT_VER=1
 export TOR_PT_SERVER_BINDADDR=obfs4-0.0.0.0:9090 
 export TOR_PT_SERVER_TRANSPORTS=obfs4
 export TOR_PT_ORPORT=127.0.0.1:8080
+export TOR_PT_STATE_LOCATION=server-state/
+
+lyrebird -enableLogging -logLevel DEBUG
