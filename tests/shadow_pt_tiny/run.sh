8efc5bb59ae53230c02e3d473734eb837cbdd5f3
Anna Lorimer <annalorimer@uchicago.edu>
2024-12-03 15:04:15 -0600

    fix permissions issues with output files

--- tests/shadow_pt_tiny/run.sh
+++ tests/shadow_pt_tiny/run.sh
@@ -4,18 +4,23 @@
 
 #set -euo pipefail
 
-simdir=tornet__net-0.01__load-3.2__trial-1__pt-obfs4
+simdir=trial1
 
-tar xaf tornet__net-0.01__load-3.2__trial-1__pt-obfs4.tar.xz
+#tar -xaf tornet__net-0.01__load-3.2__trial-1__pt-obfs4.tar.xz
 
 SEED=`python3 -c 'import random; print(random.randint(0, 999999999))'`
 
 tornettools simulate \
     --shadow /opt/bin/shadow \
-    --args "--parallelism=36 --seed=${SEED} --template-directory=shadow.data.template" \
+    --args "--parallelism=32 --seed=${SEED} --template-directory=shadow.data.template" \
     --filename shadow.config.yaml \
     ${simdir}
 
+# parses the results and saves results in the simdir
 tornettools parse ${simdir}
+# generates the plots of the results
 tornettools plot ${simdir} --prefix ${simdir}/pdfs
+# goes through all the log files and compresses them
 tornettools archive ${simdir}
+
+chown -R 777 ${simdir} 
