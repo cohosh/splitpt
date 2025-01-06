45ca07febc63ee69043e825f580e5504131fb979
annalorimer <annalorimer@uchicago.edu>
2025-01-06 12:02:31 -0600

    Add TOML field for path to lyrebird binary

--- client/lib/client-config.go
+++ client/lib/client-config.go
@@ -9,6 +9,7 @@ import (
 
 type SplitPTConfig struct {
 	SplittingAlg string
+	LyrebirdPath string
 	Connections  map[string][]struct {
 		Transport string
 		Args      []string
@@ -24,6 +25,11 @@ func GetClientTOMLConfig(tomlFilename string) (*SplitPTConfig, error) {
 	if err != nil {
 		log.Printf("Error decoding TOML config")
 		return nil, err
+	}
+
+	log.Printf(config.LyrebirdPath)
+	if config.LyrebirdPath == "" {
+		return nil, errors.New("Error processing TOML: No path to lyrebird binary specified")
 	}
 
 	switch config.SplittingAlg {
